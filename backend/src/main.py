d3aea27c393fb9ed38c62f144d34f0bd9e0ea0cc
Luciano Bermudez <lucianobmecheng69@gmail.com>
2025-09-04 18:35:06 -0700

    added static

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from src.db import create_db_and_tables
 from src.routes.pokemon import router
-
+from fastapi.staticfiles import StaticFiles
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
@@ -11,4 +11,5 @@ async def lifespan(_: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
 app.include_router(router)
