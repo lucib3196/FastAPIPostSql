5bb8a9d402151ef8a5721d7573159dda0ebd6357
Luciano Bermudez <lucianobmecheng69@gmail.com>
2025-09-05 21:28:08 -0700

    Fixing slashes

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,6 +1,13 @@
 export function SplitFileNames(arr: string[]) {
-  return arr.map((value: string) => ({
-    name: value.split("\\").pop()?.split(".")[0] ?? "",
-    image_path: value.split("images").pop(),
-  }));
+  return arr.map((value: string) => {
+    // Normalize to forward slashes
+    const normalized = value.replace(/\\/g, "/");
+
+    return {
+      // Grab filename without extension
+      name: normalized.split("/").pop()?.split(".")[0] ?? "",
+      // Grab everything after "images"
+      image_path: normalized.split("images").pop() ?? "",
+    };
+  });
 }
